46ceef23cf4f23c690114c510ce34d2ec46f6112
The-littel-programer-BeBo <beboalgenral@gmail.com>
2022-08-01 16:57:01 +0200

    bebo

---
+++ JS/app.js
@@ -0,0 +1,26 @@
+// control nav in scroll
+let lastScrollTop = 0,
+    arrive = home.clientHeight/2;
+
+window.addEventListener('scroll',()=>{
+  // hide nav background in first 100vh
+  let windowPosition = window.scrollY > home.clientHeight;
+  nav.classList.toggle('nav-active',windowPosition)
+  // hide nav in scroll down
+  let scrollTop = document.documentElement.scrollTop || window.pageYOffset;
+  if(scrollTop > lastScrollTop){
+    nav.style.cssText = 'transform: translateY(-110%);'
+  }else{
+    nav.style.cssText = 'transform: translateY(0%);'
+  }
+  lastScrollTop = scrollTop;
+  // play animation
+  document.querySelectorAll('.move').forEach(sec=> {
+    if(sec.getBoundingClientRect().top >=  -arrive &&
+        sec.getBoundingClientRect().top <= arrive) {
+      sec.style.cssText = 'animation-play-state: running;';
+    }
+  })
+})
+// window.addEventListener('load',()=>{
+// })
