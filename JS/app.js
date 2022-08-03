0fc5fec9cb35eb9ad6ab8d5c156c6ac302923600
The-littel-programer-BeBo <beboalgenral@gmail.com>
2022-08-03 07:26:36 +0200

    bebo

--- JS/app.js
+++ JS/app.js
@@ -24,3 +24,18 @@ window.addEventListener('scroll',()=>{
 })
 // window.addEventListener('load',()=>{
 // })
+function User(id, username, salary) {
+  this.i = id;
+  this.u = username;
+  this.s = salary + 1000;
+}
+// class User {
+//   constructor(id, username, salary) {
+//     this.i = id;
+//     this.u = username;
+//     this.s = salary + 1000;
+//   }
+// }
+let userOne = new User(100, "Elzero", 5000);
+let userTwo = new User(101, "Hassan", 6000);
+let userThree = new User(102, "Sayed", 7000);
