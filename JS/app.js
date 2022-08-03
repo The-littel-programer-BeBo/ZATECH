761e9aefc35e1f9c0f0d46d83c958a39cdae27de
The-littel-programer-BeBo <beboalgenral@gmail.com>
2022-08-03 07:55:17 +0200

    bebo

--- JS/app.js
+++ JS/app.js
@@ -15,27 +15,15 @@ window.addEventListener('scroll',()=>{
   }
   lastScrollTop = scrollTop;
   // play animation
+  animation()
+})
+// animation sections function
+function animation(){
   document.querySelectorAll('.move').forEach(sec=> {
     if(sec.getBoundingClientRect().top >=  -arrive &&
         sec.getBoundingClientRect().top <= arrive) {
       sec.style.cssText = 'animation-play-state: running;';
     }
   })
-})
-// window.addEventListener('load',()=>{
-// })
-function User(id, username, salary) {
-  this.i = id;
-  this.u = username;
-  this.s = salary + 1000;
 }
-// class User {
-//   constructor(id, username, salary) {
-//     this.i = id;
-//     this.u = username;
-//     this.s = salary + 1000;
-//   }
-// }
-let userOne = new User(100, "Elzero", 5000);
-let userTwo = new User(101, "Hassan", 6000);
-let userThree = new User(102, "Sayed", 7000);
+window.addEventListener('load',animation())
